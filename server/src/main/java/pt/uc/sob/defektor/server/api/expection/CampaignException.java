a992d6f4998d82d4845dad42918b5d70d719c9b9
gclarobatista <gonclabat@gmail.com>
2021-09-14 23:33:20 +0100

    Added new CampaignException

---
+++ server/src/main/java/pt/uc/sob/defektor/server/api/expection/CampaignException.java
@@ -0,0 +1,8 @@
+package pt.uc.sob.defektor.server.api.expection;
+
+public class CampaignException extends Exception {
+
+    public CampaignException(String message){
+        super(message);
+    }
+}
