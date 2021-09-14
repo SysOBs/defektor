260f56578e7bb47617d72d912c94b8079478476a
gclarobatista <gonclabat@gmail.com>
2021-09-14 23:44:00 +0100

    packagePlugins.sh updated

--- scripts/packagePlugins.sh
+++ scripts/packagePlugins.sh
@@ -3,11 +3,11 @@
 mvn -f '../plugins/dev/data collector/Jaeger/pom.xml' clean package -DskipTests -U
 
 #SYSTEM CONNECTOR
-#mvn -f '../plugins/dev/system connector/Kubernetes/pom.xml' clean package -DskipTests -U
-#mvn -f '../plugins/dev/system connector/VirtualMachine/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/system connector/Kubernetes/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/system connector/VirtualMachine/pom.xml' clean package -DskipTests -U
 
 #INJEKTOR
-#mvn -f '../plugins/dev/injektor/HttpAbort/pom.xml' clean package -DskipTests -U
-#mvn -f '../plugins/dev/injektor/HttpDelay/pom.xml' clean package -DskipTests -U
-#mvn -f '../plugins/dev/injektor/MachineShutdown/pom.xml' clean package -DskipTests -U
-#mvn -f '../plugins/dev/injektor/ProcessTerminator/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/injektor/HttpAbort/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/injektor/HttpDelay/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/injektor/MachineShutdown/pom.xml' clean package -DskipTests -U
+mvn -f '../plugins/dev/injektor/ProcessTerminator/pom.xml' clean package -DskipTests -U
