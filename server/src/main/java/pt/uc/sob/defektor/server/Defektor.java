e3b21521860473cd3130dc27229b45ad94fb676e
Gonçalo Baptista <34254160+gclarobatista@users.noreply.github.com>
2021-11-12 18:44:06 +0000

    Introduced Logger

--- server/src/main/java/pt/uc/sob/defektor/server/Defektor.java
+++ server/src/main/java/pt/uc/sob/defektor/server/Defektor.java
@@ -1,5 +1,7 @@
 package pt.uc.sob.defektor.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -7,6 +9,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class Defektor {
+
+    private static Logger logger = LoggerFactory.getLogger(Defektor.class);
 
     public static void main(String[] args) {
         System.out.println("🐳 Defektor - Write once, run away");
