4c6e343d6b8017fbeec05ab974c9d76b00e6df00
Gonçalo Baptista <34254160+gclarobatista@users.noreply.github.com>
2021-11-15 01:56:31 +0000

    Minor changes in Status classes

--- server/src/main/java/pt/uc/sob/defektor/server/orchestrator/campaign/CampaignStatus.java
+++ server/src/main/java/pt/uc/sob/defektor/server/orchestrator/campaign/CampaignStatus.java
@@ -1,10 +1,5 @@
 package pt.uc.sob.defektor.server.orchestrator.campaign;
 
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-@RequiredArgsConstructor
-@ToString
 public enum CampaignStatus {
     WAITING_TO_START,
     RUNNING,
--- server/src/main/java/pt/uc/sob/defektor/server/orchestrator/campaign/injection/run/RunStatus.java
+++ server/src/main/java/pt/uc/sob/defektor/server/orchestrator/campaign/injection/run/RunStatus.java
@@ -1,10 +1,5 @@
 package pt.uc.sob.defektor.server.orchestrator.campaign.injection.run;
 
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-@RequiredArgsConstructor
-@ToString
 public enum RunStatus {
     WAITING_TO_START,
     RUNNING_GOLDEN_RUN,
