e5d3fae1c786362db9507bc62060447cd5207361
Gonçalo Baptista <34254160+gclarobatista@users.noreply.github.com>
2022-02-11 15:49:54 +0000

    AWS Plugin

--- plugins/dev/system connector/AWS/src/main/java/pt/uc/sob/defektor/plugins/system/aws/AWSSystemPlug.java
+++ plugins/dev/system connector/AWS/src/main/java/pt/uc/sob/defektor/plugins/system/aws/AWSSystemPlug.java
@@ -2,25 +2,21 @@ package pt.uc.sob.defektor.plugins.system.aws;
 
 import com.amazonaws.services.fis.AWSFIS;
 import com.amazonaws.services.fis.AWSFISClientBuilder;
-import com.amazonaws.services.fis.model.ExperimentTemplate;
-import com.amazonaws.services.fis.model.ListActionsRequest;
-import com.amazonaws.services.fis.model.ListActionsResult;
 import com.amazonaws.services.fis.model.StartExperimentRequest;
-import org.json.JSONObject;
-import pt.uc.sob.defektor.common.com.data.TargetType;
-import pt.uc.sob.defektor.common.com.sysconfigs.SystemConfigs;
-import pt.uc.sob.defektor.common.pluginterface.SystemConnectorPlug;
+import pt.uc.sob.defektor.common.com.data.target_types.AwsTargetType;
+import pt.uc.sob.defektor.common.com.data.target_types.TargetType;
+import pt.uc.sob.defektor.common.plugin_interface.SystemConnectorPlug;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 
 public class AWSSystemPlug extends SystemConnectorPlug {
 
     AWSFIS client;
 
-    public AWSSystemPlug(SystemConfigs configs) {
-        super(configs);
+    public AWSSystemPlug(Map<String, String> configuration) {
+        super(configuration);
         client = AWSFISClientBuilder.defaultClient();
     }
 
@@ -34,10 +30,7 @@ public class AWSSystemPlug extends SystemConnectorPlug {
 
     @Override
     public List<TargetType> getTargetTypes() {
-        return new ArrayList<>() {
-            {
-            }
-        };
+        return List.of(AwsTargetType.values());
     }
 
     public void startExperiment(String experimentTemplateId, Map<String, String> tags) {
@@ -48,6 +41,10 @@ public class AWSSystemPlug extends SystemConnectorPlug {
 
         client.startExperiment(experimentRequest);
     }
+
+//    public static void main(String[] args) {
+//        System.out.println(new AWSSystemPlug(null).getTargetTypes());
+//    }
 }
 
 
