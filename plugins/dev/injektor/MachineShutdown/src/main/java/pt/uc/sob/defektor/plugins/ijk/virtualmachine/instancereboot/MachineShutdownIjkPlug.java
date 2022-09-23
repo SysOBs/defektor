9b1325fd9c12066686d51f44fa01f4307c75371d
Gonçalo Baptista <gonclabat@gmail.com>
2022-09-23 19:04:29 +0100

    Clean MachineShutdown plugin

--- plugins/dev/injektor/MachineShutdown/src/main/java/pt/uc/sob/defektor/plugins/ijk/virtualmachine/instancereboot/MachineShutdownIjkPlug.java
+++ plugins/dev/injektor/MachineShutdown/src/main/java/pt/uc/sob/defektor/plugins/ijk/virtualmachine/instancereboot/MachineShutdownIjkPlug.java
@@ -1,17 +1,16 @@
 package pt.uc.sob.defektor.plugins.ijk.virtualmachine.instancereboot;
 
 import lombok.SneakyThrows;
-import pt.uc.sob.defektor.common.com.data.Target;
-import pt.uc.sob.defektor.common.com.data.target_types.TargetType;
-import pt.uc.sob.defektor.common.com.exception.CampaignException;
-import pt.uc.sob.defektor.common.plugin_interface.InjektorPlug;
-import pt.uc.sob.defektor.common.plugin_interface.SystemConnectorPlug;
+import pt.uc.sob.defektor.common.config.InjektorParams;
+import pt.uc.sob.defektor.common.data.Target;
+import pt.uc.sob.defektor.common.data.target_types.SshEnabledTargetType;
+import pt.uc.sob.defektor.common.data.target_types.TargetType;
+import pt.uc.sob.defektor.common.exception.CampaignException;
+import pt.uc.sob.defektor.common.plugin.abstraction.InjektorPlug;
+import pt.uc.sob.defektor.common.plugin.abstraction.SystemConnectorPlug;
 import pt.uc.sob.defektor.plugins.system.virtualmachine.VMSystemPlug;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 
 public class MachineShutdownIjkPlug extends InjektorPlug<VMSystemPlug> {
 
@@ -27,16 +26,16 @@ public class MachineShutdownIjkPlug extends InjektorPlug<VMSystemPlug> {
     }
 
     @Override
-    public void performInjection(Map<String, String> parameters) throws CampaignException {
-//        Param param = Utils.jsonToObject(ijkParams.getJsonIjkParams().toString());
-//        new Thread(
-//                () -> {
-//                    while (true) {
-//                        system.sendSSHCommand(SHUTDOWN_COMMAND);
-//                        sleep(param.getInterval());
-//                    }
-//                }
-//        ).start();
+    public void performInjection(InjektorParams parameters) throws CampaignException {
+        Param param = Utils.jsonToObject(parameters.toString());
+        new Thread(
+                () -> {
+                    while (true) {
+                        system.sendSSHCommand(SHUTDOWN_COMMAND);
+                        sleep(param.getInterval());
+                    }
+                }
+        ).start();
     }
 
     @SneakyThrows
@@ -52,7 +51,7 @@ public class MachineShutdownIjkPlug extends InjektorPlug<VMSystemPlug> {
     public List<TargetType> getTargetTypes() {
         return new ArrayList<>() {
             {
-                add(TargetType.);
+                add(SshEnabledTargetType.INSTANCE);
             }
         };
     }
